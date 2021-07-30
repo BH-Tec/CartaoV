c07ab91deec9a0eeb740a1b209b1faf573dac75d
Bruno <47230010+BrunoDorea@users.noreply.github.com>
2021-07-30 09:42:49 -0300

    commit
    configuração do room database

--- app/src/main/java/br/brunodorea/cartaov/App.kt
+++ app/src/main/java/br/brunodorea/cartaov/App.kt
@@ -1,6 +1,10 @@
 package br.brunodorea.cartaov
 
 import android.app.Application
+import br.brunodorea.cartaov.data.AppDatabase
+import br.brunodorea.cartaov.data.BusinessCardRepository
 
 class App : Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
